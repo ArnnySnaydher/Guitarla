f456f554d887e804aef421c140d39d72f9421ddc
Aryans <quispeloayza18@hotmail.com>
2024-02-16 09:34:41 -0500

    usando eventos : test

--- src/components/Guitarra.vue
+++ src/components/Guitarra.vue
@@ -1,10 +1,18 @@
 <script setup>
+import { ref } from 'vue';
+
+const numero = ref(0)
     const props = defineProps({
         guitarra:{
             typeof:Object,
             required:true
         }
     })
+
+
+const increment = () =>{
+    numero.value++
+}
 </script>
 <template>
     <div class="col-md-6 col-lg-4 my-4 row align-items-center">
@@ -14,11 +22,13 @@
                 </div>
                 <div class="col-8">
                     <h3 class="text-black fs-4 fw-bold text-uppercase">{{guitarra.nombre}}</h3>
+                    <p>{{numero}}</p>
                     <p>{{guitarra.descripcion}}</p>
                     <p class="fw-black text-primary fs-3">${{ guitarra.precio }}</p>
                     <button 
                         type="button"
                         class="btn btn-dark w-100 "
+                        v-on:click="increment"
                     >Agregar al Carrito</button>
                 </div>
             </div><!-- FIN GUITARRA -->
